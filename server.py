c42f9c0d3f29589166a5e7c547e065b29d23bb8b
RED <rhabidpmendoza@gmail.com>
2024-10-01 22:06:51 +0800

    nabura pala

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/check_url', methods=['POST'])
+def check_url():
+
+    data = request.json
+    url = data.get('url', '')
+    
+    if "123" in url:
+        return jsonify({"result": 1})
+    else:
+        return jsonify({"result": 0})
+
+if __name__ == '__main__':
+    app.run(port=5000)
