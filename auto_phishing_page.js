03e35078e02951675f491f200f2a4dce26ec415a
RED <rhabidpmendoza@gmail.com>
2024-10-04 09:36:49 +0800

    auto files done

--- auto_phishing_page.js
+++ auto_phishing_page.js
@@ -1,57 +1,70 @@
 document.addEventListener("DOMContentLoaded", function (){
 
+    // Get components in html
     const received_url = document.getElementById("received-url");
-   
     const block_button = document.getElementById("block-button");
-    const allowed = document.getElementById("allowed-button");
+    const allowed_button = document.getElementById("allowed-button");
     const ignore_button = document.getElementById("ignore-button");
 
+    // Get the url sent
     const params = new URLSearchParams(window.location.search);
     const url = params.get("url");
     
+    // Display the url
     received_url.innerHTML = url;
 
+    // Block the url navigated
+    function blockURL(){
 
-    function saveURL(url){
-        chrome.storage.local.get("blockedUrls", (result) =>{
+        // Get blocked urls
+        chrome.storage.local.get("blockedUrls", (result) => {
             const blockedUrls = result.blockedUrls || [];
     
+            // Push the url to the list and go to blocked page
             if(!blockedUrls.includes(url)){
                 blockedUrls.push(url);
-                chrome.storage.local.set({ blockedUrls }, () =>{
-                    chrome.runtime.sendMessage({ action: "applyBlockingRules" }, () =>{
+                chrome.storage.local.set({ blockedUrls }, () => {
+                    chrome.runtime.sendMessage({ action: "applyBlockingRules" }, () => {
                         window.location.href = `auto_blocked_page.html?url=${encodeURIComponent(url)}`;
                     });
                 });
             }
+
         });
+
     }
 
-    // Store URL to allow
-    function allowURL(url){
+    // Store the url to allow user to visit it
+    function allowURL(){
+
+        // Get allowed urls
         chrome.storage.local.get("allowedUrls", (result) => {
             const allowedUrls = result.allowedUrls || [];
 
+            // Push the url to list so user can visit it
             if (!allowedUrls.includes(url)) {
                 allowedUrls.push(url);
                 chrome.storage.local.set({ allowedUrls }, () => {
                     window.location.href = url; 
                 });
             }
+
         });
+
     }
 
-
-
+    // Add url to blocked
     block_button.addEventListener("click", function (){
-        saveURL(url);
+        blockURL();
     });
 
-    allowed.addEventListener("click", function () {
-        allowURL(url);
+    // List the url in allowed
+    allowed_button.addEventListener("click", function (){
+        allowURL();
     });
 
-    ignore_button.addEventListener("click", function () {
+    // Close the page
+    ignore_button.addEventListener("click", function (){
         window.close();
     });
     
