bbf5f52237c80ac925fff69ff6c5cd8b02ebbb7b
RED <rhabidpmendoza@gmail.com>
2024-12-02 23:50:14 +0800

    updated --hosting api version

--- background.js
+++ background.js
@@ -53,7 +53,7 @@ function checkUrl(url, tab_id){
                     chrome.tabs.update(tab_id, { url: chrome.runtime.getURL("pages/page_loading.html") });
 
                     // Send the url to python for checking
-                    fetch("http://localhost:5000/check_url", {
+                    fetch("https://smellsphishy-api.onrender.com/check_url", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json",
